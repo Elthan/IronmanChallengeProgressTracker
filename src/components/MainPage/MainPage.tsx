4305ad1d97ffea6ef8dbe23cf6c099c5cedf45d5
Jonas Sandbekk <Elthan@users.noreply.github.com>
2022-08-05 17:29:36 +0200

    Revert "upgrade react-scripts to v.5 for tailwindcss + add navbar component"

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Navbar from "../Navigation/Navbar";
 
 class MainPage extends React.Component {
     render () {
         return (
             <div>
-                <Navbar />
                 <h1>Title</h1>
             </div>
         );
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'regenerator-runtime/runtime';
 import App from './App';
-import "./index.css";
 
 ReactDOM.render(
   <App />,
