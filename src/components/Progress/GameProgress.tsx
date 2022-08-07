63c4e9680d3fd141a9892d6424c4681134a99c55
Jonas Sandbekk <shaitan25@gmail.com>
2022-08-07 12:14:38 +0200

    Updated styling

--- src/components/Progress/GameProgress.tsx
+++ src/components/Progress/GameProgress.tsx
@@ -3,8 +3,8 @@ import IGameProgress from './IGameProgress';
 
 export default function GameProgress({gameName}: IGameProgress) {
     return (
-        <div className="flex-basis-2/12">
-            {gameName}
+        <div>
+            <p>{gameName}</p>
         </div>
     )
 }
--- src/modules/Home.tsx
+++ src/modules/Home.tsx
@@ -4,12 +4,20 @@ import GameProgress from '../components/Progress/GameProgress';
 
 function Home() {
     return (
-        <div className="text-center">
-            <div><OverallProgress /></div>
-            <div>
-                <GameProgress gameName='Valorant' />
-                <GameProgress gameName='Teamfight Tactics' />
-                <GameProgress gameName='Apex Legends' />
+        <div className="flex flex-col place-items-center w-full mx-4 my-4">
+            <div className="w-screen order-1">
+                <OverallProgress />
+            </div>
+            <div className="flex flex-row justify-start order-2 space-x-4">
+                <div className="order-1">
+                    <GameProgress gameName='Valorant' />
+                </div>
+                <div className="order-2">
+                    <GameProgress gameName='Teamfight Tactics' />
+                </div>
+                <div className="order-3">
+                    <GameProgress gameName='Apex Legends' />
+                </div>
             </div>
         </div>
     );
