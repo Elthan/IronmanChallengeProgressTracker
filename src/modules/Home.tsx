7746ae8799feb5be0f5bbaf43ae42ab5105689fc
Jonas Sandbekk <shaitan25@gmail.com>
2022-08-10 02:34:52 +0200

    Updated to work better on mobile

--- src/modules/Home.tsx
+++ src/modules/Home.tsx
@@ -37,11 +37,11 @@ export default function Home() {
 
     return (
         <div className="flex flex-col place-items-center mx-4 my-4">
-            <div className="order-1 p-4 h-24">
+            <div className="order-1 p-4 h-40 text-center sm:h-24">
                 <div className="text-3xl font-semibold">
                     <p>IRONMAN CHALLENGE PROGRESS</p>
                 </div>
-                <div className="text-center text-slate-600">
+                <div className="text-slate-600">
                     <p>Current games</p>
                 </div>
             </div>
