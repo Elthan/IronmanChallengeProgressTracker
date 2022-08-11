dae0217816c78a2d243c5151e5e99ee4d0ece034
Thomas <suncharn.pipithkul@gmail.com>
2022-08-11 01:47:58 -0400

    Image carousel for games section + hover effect

--- src/components/AboutMe/Sections/GamesSection.tsx
+++ src/components/AboutMe/Sections/GamesSection.tsx
@@ -1,16 +1,69 @@
 import React from "react";
 import SectionProps from "./SectionProps";
+import BorderDecoration from "../../Decorations/BorderDecoration";
+import RightAngledTriangle from "../../svgComponents/RightAngledTriangle";
+import Slider from "react-slick";
 
 
 function GamesSection(props: SectionProps) {
+  const settings = {
+    fade: false,
+    infinite: true,
+    autoplay: true,
+    centerPadding: '100px',
+    autoplaySpeed: 4000,
+    cssEase: "linear",
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          swipeToSlide: true,
+        }
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
+  }
+
   return (
-    <section className={`${props.cName} relative bg-zinc-800 pt-16 pb-20 px-4 lg:pt-40 lg:pb-36 overflow-hidden`}>
-      <h1 className="text-zinc-50 font-bold text-5xl md:text-7xl">The Games</h1>
-      <div className="flex flex-row justify-around w-full">
-        <img className="w-1/3" src="/images/valo_thumbnail.jpg" alt="" />
-        <img className="w-1/3" src="https://www.futuregamereleases.com/wp-content/uploads/2019/07/teamfight-tactics.jpg" alt="" />
-        <img className="w-1/3" src="https://media.contentapi.ea.com/content/dam/apex-legends/images/2019/01/apex-featured-image-16x9.jpg.adapt.crop16x9.1023w.jpg" alt="" />
-      </div>
+    <section className={`${props.cName} relative bg-zinc-800 pt-36 pb-32 px-4 lg:pt-40 lg:pb-40 overflow-hidden`}>
+      <BorderDecoration noTop />
+      <article className="px-8">
+        <h1 className="text-zinc-50 font-bold text-6xl md:text-8xl text-center mb-16">The Games</h1>
+        <Slider {...settings}>
+          <div className="relative group">
+            <div className="overflow-hidden">
+              <img className="object-cover transition-all group-hover:scale-125" src="/images/valo_thumbnail.jpg" alt="Valorant thumbnail" />
+            </div>
+            <h4 className="text-zinc-200 font-semibold text-xl mt-1 ml-2" >Valorant</h4>
+          </div>
+          <div className="relative group ">
+            <div className="overflow-hidden">
+              <img className="object-cover transition-all group-hover:scale-125" src="/images/tft_thumbnail.jpg" alt="TFT thumbnail" />
+            </div>
+            <h4 className="text-zinc-200 font-semibold text-xl mt-1 ml-2">Teamfight Tactics</h4>
+          </div>
+          <div className="relative group ">
+            <div className="overflow-hidden">
+              <img className="object-cover transition-all group-hover:scale-125" src="/images/apex_thumbnail.jpg" alt="Apex thumbnail" />
+            </div>
+            <h4 className="text-zinc-200 font-semibold text-xl mt-1 ml-2">Apex Legends</h4>
+          </div>
+        </Slider>
+      </article>
+
+      <RightAngledTriangle width="54" height="40" cName="hidden lg:block absolute bottom-0 right-0 fill-zinc-50 scale-x-[-1]"/>
+      <RightAngledTriangle width="32" height="30" cName="block lg:hidden absolute bottom-0 right-0 fill-zinc-50 scale-x-[-1]" />
     </section>
   );
 }
--- src/components/Decorations/BorderDecoration.tsx
+++ src/components/Decorations/BorderDecoration.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 
+interface BorderProps {
+  noLeft?: boolean
+  noTop?: boolean
+}
 
-function BorderDecoration() {
+
+function BorderDecoration({ noLeft, noTop }: BorderProps) {
   return (
     <>
-      <div className="absolute top-0 left-0 h-full w-8 border-zinc-200 border-r-[1px] lg:w-12"></div>
-      <div className="absolute top-0 left-0 w-4/5 h-16 border-zinc-200 border-b-[1px] "></div>
+      { !noLeft && <div className="absolute top-0 left-0 h-full w-8 border-zinc-200 border-r-[1px] lg:w-12"></div> }
+      { !noTop && <div className="absolute top-0 left-0 w-4/5 h-16 border-zinc-200 border-b-[1px] "></div> }
     </>
   );
 }
