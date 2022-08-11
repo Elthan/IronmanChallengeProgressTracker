d15a72fc8d14cf6fdba69546b60da472feaf10d8
Jonas Sandbekk <shaitan25@gmail.com>
2022-08-11 20:20:53 +0200

    Fixed minor issues and updated Home with matching background-color

--- src/modules/Home.tsx
+++ src/modules/Home.tsx
@@ -53,25 +53,25 @@ export default function Home() {
     }
 
     return (
-        <div className="flex flex-col place-items-center overflow-hidden h-[calc(100vh-64px)] m-t-4">
+        <div className="flex flex-col place-items-center h-[calc(100vh-4rem)] m-t-4">
             <div className={`order-1 ${ showStream ? `h-full` : `sm:h-16 md:h-80`} text-center w-screen`}>
                 <iframe title="twitch-stream" src={`https://player.twitch.tv/?channel=edisonparklive&parent=${window.location.hostname}&muted=true&autoplay=false`}
-                    className="w-screen md:w-screen md:h-[calc(100vh-128px)]"
+                    className="w-screen md:w-screen md:h-[calc(100vh-8rem)]"
                     allowFullScreen>
                 </iframe>
             </div>
-            <div className="order-2 w-full py-1 sm:py-3 bg-primary-700 flex justify-center text-white hidden md:flex">
+            <div className={`order-2 w-full py-1 ${ showStream ? '' : 'sm:py-3' } h-24 bg-primary-700 flex justify-center text-white hidden md:flex`}>
                 <button onClick={toggleShowStream}>
                     <svg xmlns="http://www.w3.org/2000/svg" className={`${ showStream ? 'rotate-180' : ''} h-12 w-12`} fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z" />
                     </svg>
                 </button>
             </div>
-            <div className={`order-3 content-center w-screen h-full ${ showStream ? 'hidden' : 'block' }`}>
+            <div className={`order-3 content-center bg-zinc-50 w-screen pt-2 h-full flex-row justify-center ${ showStream ? 'hidden' : 'flex' }`}>
                 {error ? <p>An error occured when fetching the data!</p> :
                     isLoading ?
                     (
-                        <div className="flex flex-row justify-center bg-primary-200 space-x-4 text-center h-full overflow-hidden pb-4 border-primary-200">
+                        <div className="flex flex-row justify-center space-x-4 text-center h-full w-full overflow-hidden pb-4 border-primary-200">
                             <LoadingProgress />
                             <LoadingProgress />
                             <LoadingProgress />
@@ -79,7 +79,7 @@ export default function Home() {
                     )
                     :
                     (
-                        <div className="flex flex-row justify-center bg-primary-200 space-x-4 text-center h-full overflow-hidden pb-4 border-primary-200">
+                        <div className="flex flex-row justify-center space-x-4 text-center h-full w-full overflow-hidden pb-4 border-primary-200">
                             {gameInfo.map((value, index) => (
                                 <GameProgress
                                     index={index}
