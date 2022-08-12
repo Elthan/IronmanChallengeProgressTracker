10088ce22306e1675c63c0e4a5085819e8de1b19
Jonas Sandbekk <shaitan25@gmail.com>
2022-08-12 12:56:46 +0200

    Temp save

--- src/components/Progress/StreamStats.tsx
+++ src/components/Progress/StreamStats.tsx
@@ -1,25 +1,49 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { BASEURL } from "../../constants/API";
+import { START_TIME_UTC } from "../../constants/Challenge";
 
 export default function StreamStats() {
     const [uptime, setUptime] = useState();
+    const [days, setDays] = useState(0);
+    const [hours, setHours] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [seconds, setSeconds] = useState(0);
+
+    // useEffect(() => {
+    //     async function fetchUptime() {
+    //         try {
+    //             const { data } = await axios.get<any>(
+    //                 `${BASEURL}/potter-ironman-uptime`
+    //             );
+
+    //             setUptime(data);
+    //         } catch (err: any) {
+    //             console.log(`Error occurred: ${err}`)
+    //         }
+    //     }
+    //     fetchUptime();
+    //     // setInterval(() => {fetchUptime()}, 3000);
+    // }, []);
 
     useEffect(() => {
-        async function fetchUptime() {
-            try {
-                const { data } = await axios.get<any>(
-                    `${BASEURL}/potter-ironman-uptime`
-                );
+        function calculateUptime() {
+            let delta = new Date(START_TIME_UTC).valueOf();
+            let days = delta / 86400 % 24;
+            delta -= days * 86400;
 
-                setUptime(data);
-            } catch (err: any) {
-                console.log(`Error occurred: ${err}`)
-            }
+            let hours = delta / 3600 % 60;
+            delta -= hours * 3600;
+
+            let minutes = delta / 60 % 60;
+            delta -= minutes * 60;
+
+            setDays(days);
+            setHours(hours);
+            setMinutes(minutes);
+            setSeconds(delta);
         }
-        fetchUptime();
-        // setInterval(() => {fetchUptime()}, 3000);
-    }, []);
+    });
 
     return (
         <div className="flex flex-col">
@@ -27,9 +51,7 @@ export default function StreamStats() {
                 STATS
             </div>
             <div className="order-2 pt-2">
-                { uptime === undefined ? "Time since start of challenge - "                  
-                : uptime
-                }
+                { uptime === undefined ? "Time since start of challenge - " : uptime }
             </div>
         </div>
     )
