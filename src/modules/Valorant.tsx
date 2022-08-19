ccb532c50935314f81c7394bdc73dedc68768e32
Thomas <suncharn.pipithkul@gmail.com>
2022-08-19 03:44:55 -0400

    scrap recharts because it might not be responsive

--- src/modules/Valorant.tsx
+++ src/modules/Valorant.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+const data = [
+  {date: 'Aug 05', rank: 'Ass 1', mmr: 100}, 
+  {date: 'Aug 07', rank: 'Ass 1', mmr: 200}, 
+  {date: 'Aug 07', rank: 'Ass 1', mmr: 300}, 
+  {date: 'Aug 08', rank: 'Ass 1', mmr: 200}, 
+  {date: 'Aug 09', rank: 'Ass 1', mmr: 500}, 
+  {date: 'Aug 10', rank: 'Ass 1', mmr: 600}, 
+];
+
+
 
 function Valorant() {
     return (
-        <div className="text-center text-white">
-            <h1><em>Stop looking at me!</em></h1>
+        <div className="text-center">
+          <section className="bg-zinc-100 p-6">
+
+          </section>
         </div>
     );
 }
