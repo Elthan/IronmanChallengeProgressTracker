4777c6ad8653c2ceef99bb0e097f38b0a913c247
Thomas <suncharn.pipithkul@gmail.com>
2022-08-19 04:25:04 -0400

    try recharts again because responsive is working

--- src/modules/Valorant.tsx
+++ src/modules/Valorant.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer } from "recharts";
 const data = [
   {date: 'Aug 05', rank: 'Ass 1', mmr: 100}, 
   {date: 'Aug 07', rank: 'Ass 1', mmr: 200}, 
@@ -8,13 +9,29 @@ const data = [
   {date: 'Aug 10', rank: 'Ass 1', mmr: 600}, 
 ];
 
-
+const customizedAxisTick = (props: {x: number; y: number; payload: {value: string}}) => {
+  return (
+    <g transform={`translate(${props.x},${props.y})`}>
+      <text fontSize={12} x={0} y={0} dy={16} textAnchor="end" fill="black" transform="rotate(-35)">
+        {props.payload.value}
+      </text>
+    </g>
+  );
+}
 
 function Valorant() {
     return (
         <div className="text-center">
           <section className="bg-zinc-100 p-6">
-
+            <ResponsiveContainer width='100%' height={400}>
+              <LineChart data={data} margin={{ top: 5, right: 20, bottom: 20, left: 0 }}>
+                <CartesianGrid stroke="#ccc"  strokeDasharray="5 5"/>
+                <XAxis dataKey="date" tick={customizedAxisTick}/>
+                <YAxis />
+                <Line type="monotone" dataKey="mmr" stroke="#8884d8"/>
+                <Tooltip />
+              </LineChart>
+            </ResponsiveContainer>
           </section>
         </div>
     );
